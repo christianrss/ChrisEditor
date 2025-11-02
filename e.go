f597e7631290513303a179c62ebe240f8e29a6fe
Christian Rafael <christianparadix@gmail.com>
2025-11-02 04:50:50 -0300

    Setting up project

---
+++ e.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mattn/go-runewidth"
+	"github.com/nsf/termbox-go"
+)
+
+func print_message(col, row int, fg, bg termbox.Attribute, message string) {
+	for _, ch := range message {
+		termbox.SetCell(col, row, ch, fg, bg)
+		col += runewidth.RuneWidth(ch)
+	}
+}
+
+func run_editor() {
+	err := termbox.Init()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	print_message(25, 11, termbox.ColorDefault, termbox.ColorDefault, "Chris - Code Editor")
+	termbox.Flush()
+	termbox.PollEvent()
+	termbox.Close()
+}
+
+func main() {
+	run_editor()
+}
