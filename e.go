859bffe9c0718a08f65bbe8b5afa3c64416a7d9c
Christian Rafael <christianparadix@gmail.com>
2025-11-02 15:36:23 -0300

    Display text buffer

--- e.go
+++ e.go
@@ -8,6 +8,35 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+var ROWS, COLS int
+var offsetX, offsetY int
+
+var text_buffer = [][]rune{
+	{'h', 'e', 'l', 'l', 'o'},
+	{'w', 'o', 'r', 'l', 'd'},
+}
+
+func display_text_buffer() {
+	var row, col int
+	for row = 0; row < ROWS; row++ {
+		text_bufferRow := row + offsetY
+		for col = 0; col < COLS; col++ {
+			text_bufferCol := col + offsetY
+			if text_bufferRow >= 0 && text_bufferRow < len(text_buffer) &&
+				text_bufferCol < len(text_buffer[text_bufferRow]) {
+				if text_buffer[text_bufferRow][text_bufferCol] != '\t' {
+					termbox.SetChar(col, row, text_buffer[text_bufferRow][text_bufferCol])
+				} else {
+					termbox.SetCell(col, row, rune(' '), termbox.ColorDefault, termbox.ColorGreen)
+				}
+			} else if row+offsetY > len(text_buffer)-1 {
+				termbox.SetCell(0, row, rune('*'), termbox.ColorBlue, termbox.ColorDefault)
+			}
+		}
+		termbox.SetChar(col, row, rune('\n'))
+	}
+}
+
 func print_message(col, row int, fg, bg termbox.Attribute, message string) {
 	for _, ch := range message {
 		termbox.SetCell(col, row, ch, fg, bg)
@@ -22,7 +51,14 @@ func run_editor() {
 		os.Exit(1)
 	}
 	for {
-		print_message(25, 11, termbox.ColorDefault, termbox.ColorDefault, "Chris - Code Editor")
+		COLS, ROWS = termbox.Size()
+		ROWS--
+		if COLS < 78 {
+			COLS = 78
+		}
+		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		display_text_buffer()
+		//print_message(25, 11, termbox.ColorDefault, termbox.ColorDefault, "Chris - Code Editor")
 		termbox.Flush()
 		event := termbox.PollEvent()
 		if event.Type == termbox.EventKey && event.Key == termbox.KeyEsc {
